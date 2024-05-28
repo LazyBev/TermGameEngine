2c62e0b814f5fa6c47e1d4f6817b9d864e238766
Lazy Beverage <157259616+LazyBev@users.noreply.github.com>
2024-05-28 22:39:54 +0100

    Update engine.c

--- engine.c
+++ engine.c
@@ -1,22 +1,46 @@
-#include <stdlib.h>
-#include "conway.h"
+#include "engine.h"
 
-int main() {
-    int gameChoices;
-    
-    for(;;) {
-        system("clear");
-        printf("Games:\n");
-        printf("1) Conway\n\n");
-        printf("> ");
-        scanf("%d", &gameChoices);
+void saveCursor() {
+    printf("\0337");
+}
 
-        switch(gameChoices) {
-            case 1:
-                conway();
-            default:
-                exit(1);
+void loadCursor() {
+    printf("\0338");
+}
+
+void display(Engine* engine, int Rows, int Cols) {
+    if (!isnan(engine->r) && !isnan(engine->c)) {
+        for (int y = 0; y < Rows; ++y) {
+            for (int x = 0; x < Cols; ++x) {
+                if (engine->front[y][x]) {
+                    printf("#");
+                } else {
+                    printf(".");
+                }
+            }
+            printf("\n");
+        }
+    } else {
+        fprintf(stderr, "ERROR: Init engine: initEngine();");
+    }
+}
+
+int mod(int a, int b) {
+    return (a % b + b) % b;
+}
+
+int delay(int seconds) {
+    return usleep(seconds);
+}
+
+void initEngine(Engine* engine) {
+    system("clear");
+    engine->r = ROWS;
+    engine->c = COLS;
+    for (int i = 0; i < ROWS; ++i) {
+        for (int j = 0; j < COLS; ++j) {
+            engine->front[i][j] = 0;
+            engine->back[i][j] = 0;
         }
     }
-    return 0;
 }
